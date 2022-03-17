dc866aab946a8b6b2c590f1e4c03d9441baedc2a
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2022-03-17 18:23:13 -0500

    Trabajo terminado en totalidad-ventana cerrar activa

--- students/1151810/js/personal.js
+++ students/1151810/js/personal.js
@@ -3,7 +3,7 @@ function abrirVentana() {
 }
 function cerrarVentana() {
     if (confirm("Seguro que quieres salir?"))
-        window.close();
+    document.getElementsByTagName ('html') [0] .remove ();
 }
 
 var contador = 0;
@@ -11,15 +11,11 @@ var contador = 0;
 function cambiarTamaño() {
     contador++;
     box = document.getElementsByClassName('container')[0];
-    if (contador %2 == 0) {
+    if (contador % 2 == 0) {
         box.style.width = '130vh';
         box.style.height = '90vh';
     } else {
         box.style.width = '180vh';
         box.style.height = '100vh';
     }
-    console.log(contador);
 }
-setTimeout(function () {
-    changeSize();
-}, 2000);
